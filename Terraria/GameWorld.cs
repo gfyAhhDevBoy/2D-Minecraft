e1dc361bf572d63541ac50d8582adb7b421cbd49
Tim Nikitin <tim.nikitin@posteo.de>
2025-04-20 22:17:23 +0200

    fwfew

--- Terraria/GameWorld.cs
+++ Terraria/GameWorld.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended.Tiled;
+using System.Collections.Generic;
 
 namespace Terraria
 {
@@ -8,66 +8,74 @@ namespace Terraria
     {
         public const int InitialWorldWidth = 50, InitialWorldHeight = 30;
 
-        private Block[,] map;
+        private List<List<Block>> map;
 
-        public GameWorld() {
-            map = new Block[InitialWorldWidth, InitialWorldHeight];
+        public GameWorld()
+        {
+            map = new List<List<Block>>(InitialWorldWidth);
+            for (int x = 0; x < InitialWorldWidth; x++)
+            {
+                map.Add(new List<Block>(InitialWorldHeight));
+                for (int y = 0; y < InitialWorldHeight; y++)
+                {
+                    map[x].Add(null);
+                }
+            }
         }
 
-        public Vector2 GetWorldSize() => new(map.GetLength(0), map.GetLength(1));
+        public Vector2 GetWorldSize() => new(map.Count, map[0].Count);
 
         public void GenerateWorld()
         {
-            for(int y = 0; y < InitialWorldHeight; y++)
+            for (int y = 0; y < InitialWorldHeight; y++)
             {
-                for(int x = 0; x < InitialWorldWidth; x++)
+                for (int x = 0; x < InitialWorldWidth; x++)
                 {
                     if (y < 10)
-                        map[x, y] = new Block(0, 0, new(x, y));
+                        map[x][y] = new Block(0, 0, new(x, y));
                     else
-                        map[x, y] = new Block(BlockType.Stone, BlockFlags.Solid | BlockFlags.Breakable, new(x, y));
+                        map[x][y] = new Block(BlockType.Stone, BlockFlags.Solid | BlockFlags.Breakable, new(x, y));
                 }
             }
 
-            map[20, 9] = new Block(BlockType.Stone, BlockFlags.Solid | BlockFlags.Breakable, new(20, 9));
+            map[20][9] = new Block(BlockType.Stone, BlockFlags.Solid | BlockFlags.Breakable, new(20, 9));
         }
 
         public void UpdateBlocks(GameTime gameTime)
         {
-            for (int y = 0; y < map.GetLength(1); y++)
+            for (int y = 0; y < map[0].Count; y++)
             {
-                for (int x = 0; x < map.GetLength(0); x++)
+                for (int x = 0; x < map.Count; x++)
                 {
-                    map[x, y].Update(gameTime);
+                    map[x][y].Update(gameTime);
                 }
             }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            for (int y = 0; y < map.GetLength(1); y++)
+            for (int y = 0; y < map[0].Count; y++)
             {
-                for (int x = 0; x < map.GetLength(0); x++)
+                for (int x = 0; x < map.Count; x++)
                 {
-                    map[x,y].Draw(gameTime, spriteBatch);
+                    map[x][y].Draw(gameTime, spriteBatch);
                 }
             }
         }
 
         public Block GetBlock(int x, int y)
         {
-            return map[x,y];
+            return map[x][y];
         }
 
         public void DestroyBlock(Block block)
         {
-            
+        
         }
 
         public void DestroyBlock(int x, int y)
         {
-            DestroyBlock(map[x,y]);
+            DestroyBlock(map[x][y]);
         }
-
     }
 }
